b842b7698a1d4bcede599d16f7cc8545d631ec7e
salsabilaaura11 <salsabilaaura755@gmail.com>
2026-02-13 07:44:09 +0700

    Update revisi

--- story_content/user.js
+++ story_content/user.js
@@ -2,6 +2,27 @@ function ExecuteScript(strId)
 {
   switch (strId)
   {
+      case "6XzJz7WWjq2":
+        Script1();
+        break;
   }
+}
+
+function Script1()
+{
+  // Cek apakah musik sudah jalan? Kalau belum, baru nyalakan.
+if (window.bgm === undefined) {
+    // Panggil file musiknya
+    window.bgm = new Audio('Opening.mp3'); 
+    
+    // Settingan wajib
+    window.bgm.loop = true;  // Biar muter terus
+    window.bgm.volume = 0.2; // Volume 20% (Biar gak berisik)
+    
+    // Mainkan!
+    window.bgm.play().catch(function(error) {
+        console.log("Menunggu interaksi user buat play musik...");
+    });
+}
 }
 
